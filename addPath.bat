914a7d2b3c0b05b826f874d7f3851269c7a253f8
DennisLindblom71 <dennis_lindblom@hotmail.com>
2024-01-16 09:33:52 +0100

    Update addPath.bat

--- addPath.bat
+++ addPath.bat
@@ -1,1 +1,10 @@
-dl”dsk 
+@echo off  REM Disables command echoing for cleaner output
+setlocal  REM Starts localization of environment changes to this batch session
+
+REM Sets a local variable 'path' to the current directory
+set "path=%CD%"  
+
+REM Adds the 'path' variable to the system's PATH environment variable
+setx PATH "%PATH%;%path%"
+
+endlocal  REM Ends localization, reverting any environment changes made in this batch session
